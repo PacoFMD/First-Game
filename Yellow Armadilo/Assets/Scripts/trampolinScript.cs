dac667c1112abfa2e724734350ecb1cb47befef0
Sigfred24 <38962583+Sigfred24@users.noreply.github.com>
2018-05-04 20:25:32 -0500

    Trampolin(Resorte)
    oli

---
+++ Yellow Armadilo/Assets/Scripts/trampolinScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trampolinScript : MonoBehaviour {
+    bool onTop;
+    GameObject bouncer;
+    Animator anim;
+    public Vector2 velocity;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (onTop)
+        {
+            anim.SetBool("isStepped", true);
+            bouncer = other.gameObject;
+        }
+    }
+
+    void OnTriggerEnter2D()
+    {
+        onTop = true;
+    }
+    void OnTriggerExit2D()
+    {
+        onTop = false;
+        anim.SetBool("isStepped", false);
+    }
+
+    void OnTriggerStay2D()
+    {
+        onTop = true;
+    }
+
+    void Jump()
+    {
+        bouncer.GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+}
