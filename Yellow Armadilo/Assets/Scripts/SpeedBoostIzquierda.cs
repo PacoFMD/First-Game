dbcbcf418c6d18ef719606090971cefd1a682a5a
PacoFMD <pacomonfmd@hotmail.com>
2018-05-23 21:05:55 -0500

    Speed Boost
    Prefabs de SpeedBoost a la derecha y a la izquierda, hay errores en el trampolin y en el sprite pack del armadillo

---
+++ Yellow Armadilo/Assets/Scripts/SpeedBoostIzquierda.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoostIzquierda : MonoBehaviour
+{
+
+    public float vel = 6.0f;
+    Rigidbody2D rig;
+    // Use this for initialization
+    void Start()
+    {
+        rig = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+
+            rig.AddForce(transform.up * vel, ForceMode2D.Impulse);
+
+        }
+    }
+}
