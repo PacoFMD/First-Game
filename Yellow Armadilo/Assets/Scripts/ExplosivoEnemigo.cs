03f5550250ba69376a3b57283bf86462e6e74b77
Sigfred24 <38962583+Sigfred24@users.noreply.github.com>
2018-06-02 11:17:09 -0500

    Enemigo Explosivo
    (Revisión Pendinte)

---
+++ Yellow Armadilo/Assets/Scripts/ExplosivoEnemigo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExplosivoEnemigo : MonoBehaviour {
+
+    public GameObject efectoExplosion;
+    public GameObject enemigoExplotado;
+
+    public AudioSource fuente;
+    public AudioClip sonido;
+
+	void Start () {
+        Scene escenaActual = SceneManager.GetActiveScene();
+        string nombreEscena = escenaActual.name;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Explosion();
+        }  
+    }
+
+    void Explosion()
+    {
+        StartCoroutine(ReinicioEscena());
+        Instantiate(efectoExplosion, transform.position, transform.rotation);
+        Instantiate(enemigoExplotado, transform.position, transform.rotation);
+        fuente.PlayOneShot(sonido);
+    }
+
+    IEnumerator ReinicioEscena()
+    {
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("Pruebas_Ramses");
+    }
+}
